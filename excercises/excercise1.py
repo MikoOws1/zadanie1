5372fdc9c288000d4c27993afdec2a89003758e0
Mikolaj Owsianny <83515@student.ue.poznan.pl>
2024-03-13 11:14:41 +0100

    Dodanie kola

--- excercises/excercise1.py
+++ excercises/excercise1.py
@@ -21,3 +21,13 @@ wysokosc = 10
 pole_tr = (bok_a * wysokosc)/2
 
 print("Pole trojkata wynosi " +str(pole_tr))
+
+
+# kolo
+
+r = 10
+pi_value = 3.14
+
+pole_kola = pi_value * r**2
+
+print("Pole kola wynosi " +str(pole_kola))
