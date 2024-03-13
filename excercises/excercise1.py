8643ad4f697093116a97bf3b8e989abbd849785a
Mikolaj Owsianny <83515@student.ue.poznan.pl>
2024-03-13 11:06:16 +0100

    Dodanie prostokata

--- excercises/excercise1.py
+++ excercises/excercise1.py
@@ -1,5 +1,16 @@
+# kwadrat
+ 
 bok_a = 12
 
 pole_kw = bok_a * bok_a
 
 print("Pole kwadratu wynosi " +str(pole_kw))
+
+# prostokat
+
+bok_b = 16
+
+pole_pr = bok_a * bok_b
+
+print("Pole prostkata wynosi " +str(pole_pr))
+
